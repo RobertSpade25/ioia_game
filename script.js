988a06b787293ea644a53f06d3c0e18da80fe4e8
Miles <mikewatkins2509@gmail.com>
2023-10-16 20:25:46 -0400

    Make level transition

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+L1Hint = document.getElementById("L1Hint")
+
 function generateRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -8,12 +10,16 @@ var flip = 0
 var number = generateRandom(1, 5)
 var score = 0
 function theRealWork(){
-  flip ++
-  if (flip == number){
-    score ++
-    points.innerText  = "Points: " + score
-    flip = 0
-    number = generateRandom(1, 5)
+    if (score < 10){
+    flip ++
+    if (flip == number){
+      score ++
+      points.innerText  = "Points: " + score
+      flip = 0
+      number = generateRandom(1, 5)
+      }
+  }else{
+    L1Hint.href = "leveltwo.html"
+    L1Hint.classList.remove("noLink")
   }
 }
-
